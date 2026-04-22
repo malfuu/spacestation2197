822cf154b7910ca525db2b1f49ff78495759c79d
malfuu <256192511+malfuu@users.noreply.github.com>
2026-04-23 01:55:27 +0200

    remove test particle code

--- client/src/game/atmos/mod.rs
+++ client/src/game/atmos/mod.rs
@@ -1,17 +1,12 @@
-mod particles;
-
 use bevy::prelude::*;
 
 use atmos_simulation::prelude::*;
-
-use crate::game::atmos::particles::ClientAtmosParticlesPlugin;
 
 pub(super) struct ClientAtmosPlugin;
 
 impl Plugin for ClientAtmosPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, disable_atmos_simulation)
-            .add_plugins(ClientAtmosParticlesPlugin);
+        app.add_systems(Startup, disable_atmos_simulation);
     }
 }
 
