c879be9f078b1f0589a0c23f3573ebf889dd8a0f
malfuu <256192511+malfuu@users.noreply.github.com>
2026-04-24 13:50:00 +0200

    add docs to `common` crate + deny `missing_docs`

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -1,18 +1,21 @@
 //! fair warning: this crate used to be a lot stuff before
-//! but, as I got more organized, stuff got cut from here.
+//! but, as it got more organized, stuff got cut from here.
 //! god willing, in the future this will probably just be removed
+#![deny(missing_docs)]
 
 use bevy::prelude::*;
 use serde::{Deserialize, Serialize};
 
 // TODO: investigate turning these into ASCII only 32 byte heapless strings.
+/// Identifier type for prototypes.
 pub type PrototypeId = String;
 
-/// Marks entity prototype name
+/// Marks entity prototype name.
 #[derive(Component, Clone, Reflect, Debug, Deref, Serialize, Deserialize)]
 #[component(immutable)]
 #[reflect(Component)]
 // TODO: tags could get help from hashing, like bevy's Name
 pub struct EntityTag(pub PrototypeId);
 
+/// Identifier type for tiles.
 pub type TileTag = PrototypeId;
